0170871802270dd578f39a5a05c43384d76884bc
lun3l <tlsddpfla@naver.com>
2017-09-19 20:57:05 +0900

    import dt.img from twrp -> does not boot into fastboot anymore but logcat does not work due to libnimswrap.so issue. will fix after November 16th

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -69,17 +69,18 @@ WIFI_DRIVER_FW_PATH_STA     := "/vendor/firmware/fw_bcmdhd.bin"
 
 # Additional Settings3
 BOARD_DTBTOOL_ARGS := -2
-BOARD_KERNEL_CMDLINE := sched_enable_hmp=1 console=tty60,115200,n8 androidboot.console=tty60 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk androidboot.selinux=permissive androidboot.emmc=true androidboot.hwid=TBD
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk androidboot.selinux=permissive
 ENABLE_CPUSETS := true
 BOARD_KERNEL_BASE := 0x80000000
-BOARD_KERNEL_PAGESIZE := 2048
-BOARD_MKBOOTIMG_ARGS := --tags_offset 0x00000100
-BOARD_KERNEL_SEPARATED_DT := true
+BOARD_KERNEL_PAGESIZE := 2048 
+BOARD_MKBOOTIMG_ARGS := --tags_offset 0x00000100 --ramdisk device/lenovo/PB1770M/ramdisk.img  --kernel device/lenovo/PB1770M/kernel --dt device/lenovo/PB1770M/dt.img
+
+# BOARD_KERNEL_SEPARATED_DT := true
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_RAMDISK_OFFSET     := 0x01000000
 
 # use custom mkbootimg (experimental)
-BOARD_CUSTOM_BOOTIMG_MK := device/lenovo/PB1770M/mkbootimg.mk
+# BOARD_CUSTOM_BOOTIMG_MK := device/lenovo/PB1770M/mkbootimg.mk
 
 # Display
 MAX_EGL_CACHE_KEY_SIZE := 12*1024
@@ -93,11 +94,12 @@ TARGET_CONTINUOUS_SPLASH_ENABLED := true
 USE_OPENGL_RENDERER := true
 
 # kernel
-TARGET_KERNEL_SOURCE := kernel/lenovo/PB1770M
+# TARGET_KERNEL_SOURCE := kernel/lenovo/PB1770M
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
+TARGET_PREBUILT_KERNEL := device/lenovo/PB1770M/kernel
 # maybe we are using zimage
-TARGET_USES_UNCOMPRESSED_KERNEL := true
+#TARGET_USES_UNCOMPRESSED_KERNEL := true
 TARGET_KERNEL_CONFIG := PB1770M_defconfig
 
 # CMHW
